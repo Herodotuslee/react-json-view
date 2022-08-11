790c818ad18904093f8d77c0b093b1e277c90b92
Albert Lee <herodotuslee@gmail.com>
2022-08-11 14:37:18 -0700

    add json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { Container, Col, Row } from 'react-bootstrap'
+import { Pie } from 'react-chartjs-2';
+
+
+const data = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    data: [300, 50, 100],
+    backgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56'
+    ],
+    hoverBackgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56'
+    ]
+  }]
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='forContainer'>
+   <span className='container1'>
+   <div className='pie1'>
+        <Pie data={data} />
+      </div>
+      <div className='pie2'>
+        <Pie data={data} />
+      </div>
+     
+   </span>
+
+    </Container>
   );
 }
 
